c7b97f08fcc03cdf290d9dbcc27d4d51e4e01bf3
aswani <ashwani.excellencetechnologies@info>
2020-01-07 18:31:30 +0530

    complete for review

---
+++ src/components/vacancy.vue
@@ -0,0 +1,259 @@
+<template>
+  <div>
+    <header>
+      <div class="headline"></div>
+      <b-container>
+        <b-row class="pt-3 pb-2">
+          <b-col class="d-flex justify-content-center">
+            <img src="./../img/logo.svg" height="100" />
+          </b-col>
+          <b-col class="d-flex justify-content-start pt-5">
+            <div class="d-flex pt-2">
+              <img src="./../img/star.svg" height="15" />
+              <img src="./../img/star.svg" height="15" />
+              <img src="./../img/star.svg" height="15" />
+              <img src="./../img/star.svg" height="15" />
+              <img src="./../img/star_half.svg" height="15" />
+            </div>
+            <div class="px-2 mb-0">
+              <p class="points">
+                8.4
+                <sup>/ 10</sup>
+              </p>
+            </div>
+            <div>
+              <img src="./../img/feedbackcompany.svg" height="30" />
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
+    </header>
+    <div>
+      <div class="backgroundImage"></div>
+      <b-container>
+        <b-row align-h="center" class="py-5">
+          <b-col class="d-flex justify-content-end">
+            <div class="shadow-sm mb-2 headerCard bg-white">
+              <h2 class="px-5 py-4">Bent u onze nieuwe partner?</h2>
+              <p class="px-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit. Nam consequat arcu sed nulla lacinia, quis
+                blandit eros facilisis. Mauris mattis nec sem nec
+                finibus. Nunc eget leo ut diam imperdiet vehicula
+                ac non elit. Pellentesque nisi neque, sodales nec
+                sem et, condimentum congue ligula. Integer non
+                ullamcorper lacus, sed luctus sapien.
+              </p>
+              <div class="px-5 py-4">
+                <button type="button" class="btn dropdown-toggle button">De mogelijkheden</button>
+              </div>
+            </div>
+          </b-col>
+          <b-col>
+            <div class="p-3">
+              <img src="./../img/timo_partner.svg" fluid alt="timo_partner" height="300" />
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
+
+    <div class="bg-light">
+      <b-container style="width:62%;" class="py-5">
+        <b-row class="py-3">
+          <b-col>
+            <div align="center" class="shadow-sm-sm p-2 bg-white serviceCard">
+              <div class="py-3">
+                <img src="./../img/range.png" height="50" />
+              </div>
+              <h5 class="px-3">Groot bereik</h5>
+              <p class="py-3 px-3 text-left">
+                Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Nam
+                consequat arcu sed nulla lacinia,
+                quis blandit eros facil.
+              </p>
+            </div>
+          </b-col>
+          <b-col>
+            <div align="center" class="shadow-sm p-2 bg-white serviceCard">
+              <div class="py-3">
+                <img src="./../img/freeSignup.png" height="50" />
+              </div>
+              <h5 class="px-3">Gratis aanmelden</h5>
+              <p class="py-3 px-3 text-left">
+                Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Nam
+                consequat arcu sed nulla lacinia,
+                quis blandit eros facil.
+              </p>
+            </div>
+          </b-col>
+          <b-col>
+            <div align="center" class="shadow-sm p-2 bg-white serviceCard">
+              <div class="py-3">
+                <img src="./../img/service.png" height="50" />
+              </div>
+              <h5 class="px-3">Klantenservice</h5>
+              <p class="py-3 px-3 text-left">
+                Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Nam
+                consequat arcu sed nulla lacinia,
+                quis blandit eros facil.
+              </p>
+            </div>
+          </b-col>
+        </b-row>
+        <b-row class="py-3">
+          <b-col>
+            <div align="center" class="shadow-sm p-2 bg-white serviceCard">
+              <div class="py-3">
+                <img src="./../img/support.png" height="50" />
+              </div>
+              <h5 class="px-3">Support en inzage</h5>
+              <p class="py-3 px-3 text-left">
+                Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Nam
+                consequat arcu sed nulla lacinia,
+                quis blandit eros facil.
+              </p>
+            </div>
+          </b-col>
+          <b-col>
+            <div align="center" class="shadow-sm p-2 bg-white serviceCard">
+              <div class="py-3">
+                <img src="./../img/money.png" height="50" />
+              </div>
+              <h5 class="px-3">Verdien geld</h5>
+              <p class="py-3 px-3 text-left">
+                Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Nam
+                consequat arcu sed nulla lacinia,
+                quis blandit eros facil.
+              </p>
+            </div>
+          </b-col>
+          <b-col>
+            <div align="center" class="shadow-sm p-2 bg-white serviceCard">
+              <div class="py-3">
+                <img src="./../img/quality.png" height="50" />
+              </div>
+              <h5 class="px-3">Kwalitatief aanbod</h5>
+              <p class="py-3 px-3 text-left">
+                Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Nam
+                consequat arcu sed nulla lacinia,
+                quis blandit eros facil.
+              </p>
+            </div>
+          </b-col>
+        </b-row>
+        <b-row class="py-3">
+          <b-col class="d-flex justify-content-end">
+            <div align="center" class="shadow-sm p-2 bg-white serviceCard">
+              <div class="py-3">
+                <img src="./../img/wrench.png" height="50" />
+              </div>
+              <h5 class="px-3">Maatwerk oplossingen</h5>
+              <p class="py-3 px-3 text-left">
+                Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Nam
+                consequat arcu sed nulla lacinia,
+                quis blandit eros facil.
+              </p>
+            </div>
+          </b-col>
+          <b-col>
+            <div align="center" class="shadow-sm p-2 bg-white serviceCard">
+              <div class="py-3">
+                <img src="./../img/thumbs.png" height="50" />
+              </div>
+              <h5 class="mb-4 pb-2">Gemak</h5>
+              <p class="py-3 px-3 text-left">
+                Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Nam
+                consequat arcu sed nulla lacinia,
+                quis blandit eros facil.
+              </p>
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
+    <footer class="d-flex justify-content-center">
+      <div class="d-flex justify-content-between footer">
+        <div class="pt-3">
+          <p>&copy;2013 - 2019 Vliegenenparkeren.nl - Alle rechten voorbehouden</p>
+        </div>
+        <div class="pt-3">
+          <p>Bedrijfsgegevens / Privacy policy / Algemene voorwaarden / Sitemap</p>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+<script>
+export default {
+  name: "vacancy"
+};
+</script>
+
+<style scoped>
+.headerCard {
+  max-width: 23rem;
+}
+.headerCard p {
+  font-size: 13px;
+}
+.headerCard h2 {
+  font-weight: bolder;
+}
+
+.serviceCard {
+  width: 14rem;
+  border-bottom: 10px solid #00bfff;
+}
+.serviceCard p {
+  font-size: 13px;
+  color: black;
+}
+footer {
+  background-color: #005070;
+}
+.footer {
+  width: 80%;
+}
+.footer p {
+  color: #6192a7;
+  font-size: 12px;
+  line-height: 0.8;
+}
+.points {
+  font-weight: bold;
+  font-size: 18px;
+  color: #005070;
+}
+sup {
+  color: #00bfff;
+  font-weight: lighter;
+}
+.headline {
+  position: absolute;
+  top: 100px;
+  height: 25px;
+  width: 100%;
+  background-color: #00bfff;
+  z-index: -1;
+}
+.backgroundImage {
+  clip-path: polygon(0 0, 100% 0, 100% 40%, 0 70%);
+  background-color: whitesmoke;
+  position: absolute;
+  width: 100%;
+  height: 500px;
+}
+.button{
+  background-color: #00bfff;
+  color:white;
+}
+</style>
