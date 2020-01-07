09761df4db28e825b68b7fe1d3d0e73c82e615df
aswani <ashwani.excellencetechnologies@info>
2020-01-07 18:26:45 +0530

    creation

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+ <div>
+   <vacancy/>
+ </div>
+</template>
+
+<script>
+import vacancy from '@/components/vacancy';
+
+export default {
+  name: 'App',
+
+  components: {
+    vacancy
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
